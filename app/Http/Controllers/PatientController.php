50183fcf8cf80448560d856f4218ddd23f421e4d
ZaidHilmi <zaidhilmi15@gmail.com>
2021-11-08 16:18:57 +0700

    Added: method index and show

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Patient;
 
 class PatientController extends Controller
 {
-    //
+    public function index()
+    {
+        $patients = Patient::all();
+        
+        $data = [
+            'Message' => 'Get all Patients',
+            'Data' => $patients
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    public function show($id)
+    {   
+        $patients = Patient::find($id);
+
+        if ($patients){
+            $data = [
+                'Message' => 'Get Detail Patient',
+                'Data' => $patients
+            ];
+
+            return response()->json($data, 200);
+        } else{
+            $data = [
+                'Message' => 'Data not found'
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PatientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/patients',[PatientController::class, 'index']);
+Route::get('/patients/{id}',[PatientController::class, 'show']);
