c4a770a51e50778fce3540ae7e70065b8a9d5215
ZaidHilmi <zaidhilmi15@gmail.com>
2021-11-11 21:19:22 +0700

    Added: Method store, update, and delete

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -38,4 +38,87 @@ class PatientController extends Controller
             return response()->json($data, 404);
         }
     }
+
+    public function store(Request $request)
+    {
+        $input = [
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'status' => $request->status,
+            'in_date_at' => $request->in_date_at,
+            'out_date_at' => $request->out_date_at,
+        ];
+
+        $validateData = $request->validate([
+            'name' => 'required|string',
+            'phone' => 'required',
+            'address' => 'required|string',
+            'status' => 'required',
+            'in_date_at' => 'required|date',
+            'out_date_at' => 'nullable',
+        ]);
+
+        $patients = Patient::create($input);
+        $patients = Patient::create($validateData);
+
+        $data = [
+            'message' => 'Success create data',
+            'data' => $patients
+        ];
+
+        return response()->json($patients, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $patients = Patient::find($id);
+
+        if ($patients){
+            $input = [
+                'name' => $request->name ?? $patients->name,
+                'phone' => $request->phone ?? $patients->phone,
+                'address' => $request->address ?? $patients->address,
+                'status' => $request->status ?? $patients->status,
+                'in_date_at' => $request->in_date_at ?? $patients->in_date_at,
+                'out_date_at' => $request->out_date_at ?? $patients->out_date_at
+            ];
+
+            $patients->update($input);
+
+            $data = [
+                'message' => 'Patient Updated',
+                'data' => $patients
+            ];
+
+            return response()->json($data, 200);
+        } else{
+            $data = [
+                'message' => 'Patient not found' 
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $patients = Patient::find($id);
+
+        if ($patients){
+            $patients->delete();
+
+            $data = [
+                'message' => 'Student is deleted'
+            ];
+
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Student is not found'
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/patients',[PatientController::class, 'index']);
 Route::get('/patients/{id}',[PatientController::class, 'show']);
+Route::post('/patients',[PatientController::class, 'store']);
+Route::put('/patients/{id}',[PatientController::class, 'update']);
+Route::delete('/patients/{id}',[PatientController::class, 'destroy']);
