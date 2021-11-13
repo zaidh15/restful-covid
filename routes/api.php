42bbbd29d94a2725c10c3559780f348c8e24a599
ZaidHilmi <zaidhilmi15@gmail.com>
2021-11-13 20:29:17 +0700

    Update: route api

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 # Route patients
-Route::get('/patients',[PatientController::class, 'index']);
-Route::get('/patients/{id}',[PatientController::class, 'show']);
+Route::apiResource('patients', PatientController::class);
+
+# Route search, positive, recovered, dead
 Route::get('/patients/search/{name}',[PatientController::class, 'search']);
 Route::get('/patients/status/positive',[PatientController::class, 'positive']);
 Route::get('/patients/status/recovered',[PatientController::class, 'recovered']);
 Route::get('/patients/status/dead',[PatientController::class, 'dead']);
-Route::post('/patients',[PatientController::class, 'store']);
-Route::put('/patients/{id}',[PatientController::class, 'update']);
-Route::delete('/patients/{id}',[PatientController::class, 'destroy']);
